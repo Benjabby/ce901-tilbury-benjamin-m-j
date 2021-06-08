db5d6e96334deab113fcb9c6966a5ab756fc30ed
Benjamin Tilbury <bt16726@essex.ac.uk>
2021-06-08 22:27:50 +0100

    Added improved sky mask refinement, currently untested.

--- haze-video-dataset/simplewrappers/simpleskyar.py
+++ haze-video-dataset/simplewrappers/simpleskyar.py
@@ -14,6 +14,52 @@ import importlib
 def print(*args, **kwargs):
     builtins.print("SkyAR         > ", *args, **kwargs)
 
+def special_guided(guide, target, radius, eps):
+    m, n = guide.shape
+
+    avgDenom = window_sum(np.ones(guide.shape), radius);
+    mean_g = window_sum(guide, radius) / avgDenom;
+    corr_gg = window_sum(guide * guide, radius) / avgDenom;
+    var_g = corr_gg - mean_g * mean_g;
+    mask = mean_g-var_g;
+
+    mean_t = window_sum(target, radius) / avgDenom;
+    corr_gt = window_sum(guide * target, radius) / avgDenom;
+    cov_gt = corr_gt - mean_g * mean_t;
+    a = cov_gt / (var_g + eps);
+    b = mean_t - a * mean_g;
+    mean_a = window_sum(a, radius) / avgDenom;
+    mean_b = window_sum(b, radius) / avgDenom;
+
+    q = mean_a * guide + mean_b;
+
+    qd = q;
+    qd[q>mask]=1;
+
+    return q, qd
+
+
+    
+def window_sum(image, r):
+    h, w = image.shape
+    sumImg = np.zeros(img.shape)
+
+    im_cum = np.cumsum(image, axis=0);
+
+    sumImg[:r, :] = im_cum[r:2*r, :];
+    sumImg[r+1:h-r-1, :] = im_cum[2*r+1:, :] - im_cum[:h-2*r-2, :];
+    sumImg[h-r:, :] = np.repeat(im_cum[h-1, :], [r, 1]) - im_cum[h-2*r-1:h-r-2, :];
+
+    % X axis
+    im_cum = np.cumsum(sumImg,axis=1);
+
+    sumImg[:, :r] = im_cum[:, r:2*r);
+    sumImg[:, r+1:w-r-1] = im_cum[:, 2*r+1:] - im_cum[:, :w-2*r];
+    sumImg[:, w-r:) = np.repeat(im_cum[:, w-1], [1, r]) - im_cum[:, w-2*r-1:w-r-2];
+
+    return sumImg
+
+
 class SimpleSkyFilter():
 
     def __init__(self, args):
@@ -48,6 +94,17 @@ class SimpleSkyFilter():
         
         return np.clip(refined_skymask, a_min=0, a_max=1)
 
+    def skymask_improved_refinement(self, G_pred, img):
+
+        r, eps = 20, 0.001
+        gray = np.mean(img, axis=-1)
+        
+        basic, helper = special_guided(gray, G_pred, r, eps)
+        
+        returned_skymask = guidedFilter(basic, helper, r, eps)
+        
+        return np.clip(refined_skymask, a_min=0, a_max=1)
+
 
     def synthesize(self, img_HD):
 
@@ -68,8 +125,9 @@ class SimpleSkyFilter():
 
         G_pred = G_pred[...,0]
         skymask = self.skymask_refinement(G_pred, img_HD)
+        improved = self.skymask_improved_refinement(G_pred, img_HD)
 
-        return G_pred, skymask
+        return G_pred, skymask, improved
 
 
     def cvtcolor_and_resize(self, img_HD):
@@ -94,7 +152,7 @@ class SimpleSkyFilter():
             img_HD = cv2.imread(this_dir, cv2.IMREAD_COLOR)
             img_HD = self.cvtcolor_and_resize(img_HD)
 
-            G_pred, skymask  = self.synthesize(img_HD)
+            G_pred, skymask, improved  = self.synthesize(img_HD)
 
             raw_path = os.path.join(output_dir, "raw","img", img_names[idx])
             im = (G_pred*65535)
