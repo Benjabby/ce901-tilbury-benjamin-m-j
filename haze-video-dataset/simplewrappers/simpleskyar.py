Bug fix for improved sky masking

haze-video-dataset/simplewrappers/simpleskyar.py
@@ -10,6 +10,7 @@ import builtins
 from cv2.ximgproc import guidedFilter
 import imageio
 import importlib
+from scipy.ndimage import uniform_filter
 
 def print(*args, **kwargs):
     builtins.print("SkyAR         > ", *args, **kwargs)
@@ -17,7 +18,7 @@ def print(*args, **kwargs):
 def special_guided(guide, target, radius, eps):
     m, n = guide.shape
 
-    avgDenom = window_sum(np.ones(guide.shape), radius);
+    avgDenom = window_sum(np.ones_like(guide), radius);
     mean_g = window_sum(guide, radius) / avgDenom;
     corr_gg = window_sum(guide * guide, radius) / avgDenom;
     var_g = corr_gg - mean_g * mean_g;
@@ -33,7 +34,7 @@ def special_guided(guide, target, radius, eps):
 
     q = mean_a * guide + mean_b;
 
-    qd = q;
+    qd = np.copy(q);
     qd[q>mask]=1;
 
     return q, qd
@@ -42,20 +43,18 @@ def special_guided(guide, target, radius, eps):
     
 def window_sum(image, r):
     h, w = image.shape
-    sumImg = np.zeros(img.shape)
+    sumImg = np.zeros_like(image)
 
     im_cum = np.cumsum(image, axis=0);
+    sumImg[:r+1, :] = im_cum[r:2*r+1, :]
+    sumImg[r+1:h-r, :] = im_cum[2*r+1:, :] - im_cum[:h-2*r-1, :];
+    sumImg[h-r:, :] = np.tile(im_cum[h-1, :], [r, 1]) - im_cum[h-2*r-1:h-r-1, :];
 
-    sumImg[:r, :] = im_cum[r:2*r, :];
-    sumImg[r+1:h-r-1, :] = im_cum[2*r+1:, :] - im_cum[:h-2*r-2, :];
-    sumImg[h-r:, :] = np.repeat(im_cum[h-1, :], [r, 1]) - im_cum[h-2*r-1:h-r-2, :];
-
-    % X axis
     im_cum = np.cumsum(sumImg,axis=1);
 
-    sumImg[:, :r] = im_cum[:, r:2*r);
-    sumImg[:, r+1:w-r-1] = im_cum[:, 2*r+1:] - im_cum[:, :w-2*r];
-    sumImg[:, w-r:) = np.repeat(im_cum[:, w-1], [1, r]) - im_cum[:, w-2*r-1:w-r-2];
+    sumImg[:, :r+1] = im_cum[:, r:2*r+1];
+    sumImg[:, r+1:w-r] = im_cum[:, 2*r+1:] - im_cum[:, :w-2*r-1];
+    sumImg[:, w-r:] = np.tile(im_cum[:, w-1], [r, 1]).T - im_cum[:, w-2*r-1:w-r-1];
 
     return sumImg
 
@@ -101,7 +100,7 @@ class SimpleSkyFilter():
         
         basic, helper = special_guided(gray, G_pred, r, eps)
         
-        returned_skymask = guidedFilter(basic, helper, r, eps)
+        refined_skymask, _ = special_guided(basic, helper, r, eps)
         
         return np.clip(refined_skymask, a_min=0, a_max=1)
 
@@ -157,9 +156,14 @@ class SimpleSkyFilter():
             raw_path = os.path.join(output_dir, "raw","img", img_names[idx])
             im = (G_pred*65535)
             imageio.imwrite(raw_path, im.astype(np.uint16))
+            
             refined_path = os.path.join(output_dir, "default_refined","img", img_names[idx])
             im = (skymask*65535)
             imageio.imwrite(refined_path, im.astype(np.uint16))
+
+            improved_path = os.path.join(output_dir, "improved","img", img_names[idx])
+            im = (improved*65535)
+            imageio.imwrite(improved_path, im.astype(np.uint16))
 
             if self.verbose: print("    > ",'Processed: %d / %d ...' % (idx+1, len(img_names)))
 